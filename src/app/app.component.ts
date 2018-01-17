dae8d53066466059ed1842631d96f38a5f0f1f4b
nicolasmoura <nicolas-md@hotmail.com>
2018-01-17 08:27:58 -0200

    adicionando server.js e configurações de ambiente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {UnformattedTextDto} from './model/unformatted-text-dto';
 import {FormattedTextDto} from './model/formatted-text-dto';
 import {Message} from 'primeng/primeng';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -21,17 +22,17 @@ export class AppComponent {
   public sendFormatTextRequest() {
     const body = new UnformattedTextDto(this.unformattedText, this.lineLength);
 
-    this.http.post<FormattedTextDto>('http://localhost:9876/formatted-text', body, {responseType: 'json'})
+    this.http.post<FormattedTextDto>(environment.API_ENDPOINT + '/formatted-text', body, {responseType: 'json'})
       .subscribe(response => {
         this.formattedText = response.text;
-        this.showMessage('success', 'Sucesso', 'Texto formatado. :)')
+        this.showMessage('success', 'Sucesso', 'Texto formatado. :)');
       }, err => {
         const errorMessage = JSON.parse(JSON.stringify(err)).error.message;
         this.showMessage('error', 'Erro', errorMessage);
       });
   }
 
-  private showMessage(type: string, title: string, body: string ): void {
+  private showMessage(type: string, title: string, body: string): void {
     this.messages = [];
     this.messages.push({severity: type, summary: title, detail: body});
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  API_ENDPOINT: 'https://stringbreaker-api.herokuapp.com'
 };
